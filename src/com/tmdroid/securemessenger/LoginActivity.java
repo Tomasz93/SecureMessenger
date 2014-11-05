d37f158712e824f4a2a15f22c471b4a20b8cd81b
Tomasz93 <tomasz.najda@hotmail.com>
2014-11-05 19:23:07 +0100

    Pierwsze UI

--- src/com/tmdroid/securemessenger/LoginActivity.java
+++ src/com/tmdroid/securemessenger/LoginActivity.java
@@ -1,15 +1,81 @@
 package com.tmdroid.securemessenger;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
+	TextView mIpAddressField;
+	BroadcastReceiver mWifiReceiver;
+	Context context;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = this;
+     
+        setWifiIpAddress(this);
+        
+        mWifiReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+	        	setWifiIpAddress(context);
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        super.registerReceiver(mWifiReceiver, filter);
+       
+    }
+
+    @Override
+	public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mWifiReceiver, filter);
+    }
+    
+    @Override
+	public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mWifiReceiver);
+    }
+    
+    private void setWifiIpAddress(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+
+        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+            ipAddress = Integer.reverseBytes(ipAddress);
+        }
+
+        byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+        String ipAddressString;
+        try {
+            ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
+        } catch (UnknownHostException ex) {
+            Log.e("WIFIIP", "Unable to get ip address.");
+            ipAddressString = context.getString(R.string.ip_address_error);
+        }
+        
+        mIpAddressField = (TextView) this.findViewById(R.id.ipAddressField);
+        mIpAddressField.setText(ipAddressString);
     }
 }
